c7337dff153867267c5eefceec14894543ae6e3c
madyanes <madyanes.dev@gmail.com>
2024-07-25 20:07:57 +0700

    Creating the stars

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,5 @@ import StarRating from './StarRating'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <StarRating maxRating={5} />
-    <StarRating maxRating={10} />
-    <StarRating />
   </React.StrictMode>
 )
